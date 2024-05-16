afe224063c1efb31ed1b4bc1fc3bd19e9f3371cb
Samarth-R <samarthbogar78@gmail.com>
2024-05-16 17:17:59 +0530

    model trainer changes

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from textSummarizer.pipeline.stage_05_model_evaluation import ModelEvaluationTra
 from textSummarizer.logging import logger
 
 STAGE_NAME = "Data Ingestion stage"
-
 try:
     logger.info(f">>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
